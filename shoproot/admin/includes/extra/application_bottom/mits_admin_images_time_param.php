643bd0a6ace2b4a5cc28a1a536e82057fbf3196a
hetfield74 <hetfield@merz-it-service.de>
2022-07-15 12:21:53 +0200

    Einschränkung auf Kategorie- und Produktbilder Danke an noRiddle für das Suchen der entsprechenden Klassen!

--- shoproot/admin/includes/extra/application_bottom/mits_admin_images_time_param.php
+++ shoproot/admin/includes/extra/application_bottom/mits_admin_images_time_param.php
@@ -14,7 +14,7 @@
 ?>
 <script>
   $(document).ready(function(){
-    $('img').each(function () {
+    $('.thumbnail-catimage, .thumbnail-productsimage, .categories_view_data img').each(function () {
       var curSrc = $(this).attr('src');
       $(this).attr('src', curSrc + '?t=<?php echo time();?>');
     });
